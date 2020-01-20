17a020d01417dbf4ca6dcdb396a9a51fcac01104
grisentb <tombra-7@student.ltu.se>
2020-01-20 10:19:07 +0100

    Add unity repository

---
+++ Assets/_Scripts/Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	CharacterController CC;
+	public float speed = 10f;
+	private Vector3 moveVector = Vector3.zero;
+	public float sensitivity = 1.0f;
+	float verticalRotation = 0;
+	public float upDownRange = 70.0f;
+	public float jump = 10.0f;
+	float grav = 0;
+    // Start is called before the first fr
+    // Start is called before the first frame update
+    void Start()
+    {
+        CC = GetComponent<CharacterController>();
+        Screen.lockCursor = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mouse Input
+		float rotLeftRight = Input.GetAxis("Mouse X") * sensitivity;
+		transform.Rotate(0, rotLeftRight, 0);
+
+		
+		verticalRotation -= Input.GetAxis("Mouse Y") * sensitivity;
+		verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+		Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+		//WASD Movement
+
+		if(!CC.isGrounded)
+		{
+			grav += Physics.gravity.y * Time.deltaTime;
+        }
+        moveVector = new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime, grav, Input.GetAxis("Vertical") * Time.deltaTime);
+        moveVector = transform.rotation * moveVector * speed;
+        CC.Move(moveVector);
+    }
+}
---
+++ Assets/_Scripts/PlayerLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+	public float sensitivity = 1.0f;
+	float verticalRotation = 0;
+	public float upDownRange = 70.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	Screen.lockCursor = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {	
+        
+		float rotLeftRight = Input.GetAxis("Mouse X") * sensitivity;
+		transform.Rotate(0, rotLeftRight, 0);
+
+		
+		verticalRotation -= Input.GetAxis("Mouse Y") * sensitivity;
+		verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+		Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+    }
+}
