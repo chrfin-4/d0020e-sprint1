4cdea8119bb44451c9437c9c85bb1ad37ca13ddc
Josef Utbult <josutb-7@student.ltu.se>
2020-01-30 21:28:26 +0100

    Fixed input handler

---
+++ Assets/ControllerInputHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInputHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+        // Update is called once per frame
+    void Update()
+    {
+        OVRInput.Update();
+        float trigger = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
+
+        GetComponent<Renderer>().material.color = new Color(255 - trigger * 25, 255 - trigger * 25, 255);
+        
+    }
+
+    void FixedUpdate()
+    {
+        OVRInput.FixedUpdate();
+    }
+}
---
+++ Assets/ovrManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ovrManager : MonoBehaviour
+{	public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OVRInput.Update();
+        if(OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) >= 0.5f){
+        	Instantiate(prefab, new Vector3(375, 170, -419), Quaternion.identity);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        OVRInput.FixedUpdate();
+    }
+}
